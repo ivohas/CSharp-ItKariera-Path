1ca579aa104ac95a7ad476cb5f8e209c7c56b3fe
ivohas <105879092+ivohas@users.noreply.github.com>
2022-11-08 19:28:30 +0200

    Create Program.cs

---
+++ 01. Introduction/AreaOfRentagular/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace AreaOfRentagular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal a = decimal.Parse(Console.ReadLine());
+            decimal b = decimal.Parse(Console.ReadLine());
+            var area = a * b;
+            Console.WriteLine(area);
+        }
+    }
+}
