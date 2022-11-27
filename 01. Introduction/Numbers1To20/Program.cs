9dabb0115edd93e135c89ed9fd06f83cb79f5550
ivohas <105879092+ivohas@users.noreply.github.com>
2022-11-27 12:05:50 +0200

    Create Program.cs

---
+++ 01. Introduction/Numbers1To20/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Numbers1To20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
