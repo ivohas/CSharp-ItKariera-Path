16960db94596ec18f4ff87b0890a393501a29acb
ivohas <105879092+ivohas@users.noreply.github.com>
2022-12-22 16:58:58 +0200

    Create Program.cs

---
+++ 01. Introduction/TreeFromStars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TreeFromStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stars = "";
+            for (int i = 0; i < 11; i++)
+            {
+                stars += "*";
+                Console.WriteLine(stars);
+            }
+        }
+    }
+}
