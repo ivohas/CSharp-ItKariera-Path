f5a62a23876b7e1cd235fee6d1858d28f3c14421
ivohas <105879092+ivohas@users.noreply.github.com>
2023-01-01 12:23:16 +0200

    Create Program.cs

---
+++ 02. Calculate, Operator, Expressions/ConcatinateByName/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ConcatinateByName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var firstName= Console.ReadLine();
+
+            var lastName= Console.ReadLine();
+
+            var age = Console.ReadLine();
+             
+            var town = Console.ReadLine();
+            Console.WriteLine($"You are {firstName} {lastName}, a {age}-years old person from {town}. ");
+        }
+    }
+}
