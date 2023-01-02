7e5a83fd7921e1a9d6f49cf05c1dbf743fc7f8d8
ivohas <105879092+ivohas@users.noreply.github.com>
2023-01-02 18:07:54 +0200

    Create Program.cs

---
+++ 02. Calculate, Operator, Expressions/Convert_C_F/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Convert_C_F
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cel = double.Parse(Console.ReadLine());
+            double far = cel * 9 / 5 + 32;
+            Console.WriteLine(far);
+        }
+    }
+}
