86aa17fb29a8adb78571e0e6392d5984141b386f
ivohas <105879092+ivohas@users.noreply.github.com>
2023-01-06 16:32:20 +0200

    Create Program.cs

---
+++ 02. Calculate, Operator, Expressions/HelloByName/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace HelloByName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
