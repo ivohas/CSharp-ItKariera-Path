36073161f1f76ae6bb8b910bb9903e2c0c956bf4
ivohas <105879092+ivohas@users.noreply.github.com>
2023-01-10 16:01:30 +0200

    Create Program.cs

---
+++ 03. Conditional checks/Excellent_Result/1000-Days/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1000_Days
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string startinDate = Console.ReadLine();
+            string format = "dd-MM-yyyy";
+            var date = DateTime.ParseExact(startinDate, format, CultureInfo.InvariantCulture);
+            date = date.AddDays(1000 - 1);
+            Console.WriteLine(date.ToString(format));
+        }
+    }
+}
