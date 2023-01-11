38844bd5096f0dc13a10f371d1d02ba00d05f509
ivohas <105879092+ivohas@users.noreply.github.com>
2023-01-11 18:37:03 +0200

    Create Program.cs

---
+++ 03. Conditional checks/Excellent_Result/AnimalClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace AnimalClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //dog -> mammal
+//• crocodile, tortoise, snake->reptile
+//• others->unknown
+            var animal = Console.ReadLine();
+            if (animal=="dog")
+            {
+                Console.WriteLine("mammal");
+            }
+            else if (animal== "crocodile"||animal== "tortoise"||animal== "snake")
+            {
+                Console.WriteLine("reptile");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+           
+        }
+    }
+}
