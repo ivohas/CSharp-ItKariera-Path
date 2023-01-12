4e84582d6c953c646135a74ac019183e268b96b3
ivohas <105879092+ivohas@users.noreply.github.com>
2023-01-12 16:30:07 +0200

    Create Program.cs

---
+++ 03. Conditional checks/Excellent_Result/BonusPoint/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace BonusPoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            double bonus = 0;
+            if (num<=100)
+            {
+                bonus += 5;
+            }
+
+            if (num<=1000&&num>100)
+            {
+                bonus = num / 5;
+            }
+
+            if (num > 1000) {
+
+                bonus = num / 10;
+            }
+
+            if (num % 2 == 0) {
+                bonus++;
+            } else if (num%5==0) {
+                bonus += 2;
+            }
+            Console.WriteLine(bonus);
+            Console.WriteLine(bonus+num);
+        }
+    }
+}
