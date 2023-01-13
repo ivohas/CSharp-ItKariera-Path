bba829c1d6f546f4fe87f940e7f7e5ad60102e25
ivohas <105879092+ivohas@users.noreply.github.com>
2023-01-13 16:59:03 +0200

    Create Program.cs

---
+++ 03. Conditional checks/Excellent_Result/Cinema/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Premiere – премиерна прожекция, на цена 12.00 лева.
+            //• Normal – стандартна прожекция, на цена 7.50 лева.
+            //• Discount – прожекция за деца, ученици и студенти на намалена цена от 5.00 лева.
+
+            string type = Console.ReadLine();
+            var rows = double.Parse(Console.ReadLine());
+            var columns = double.Parse(Console.ReadLine());
+            var places = rows * columns;
+            double price = 0;
+            switch (type)
+            {
+                case "Premiere": price = 12; break;
+                case "Normal": price = 7.5; break;
+                case "Discount": price = 5; break;
+                default:
+                    break;
+            }
+            Console.WriteLine($"{price * places}");
+
+        }
+    }
+}
