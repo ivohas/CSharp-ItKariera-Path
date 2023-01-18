553278cbc4b5156046f9cd72c91f767e5e1d6568
ivohas <105879092+ivohas@users.noreply.github.com>
2023-01-18 18:40:37 +0200

    Create Program.cs

---
+++ 03. Conditional checks/Excellent_Result/GuessPassword/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace GuessPassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var password= "s3cr3t!P@ssw0rd";
+            var guess= Console.ReadLine();
+            if (password==guess)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
