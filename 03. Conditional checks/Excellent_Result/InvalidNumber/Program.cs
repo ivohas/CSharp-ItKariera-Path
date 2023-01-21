a2457628676d280e1d601a52a88651b60464c374
ivohas <105879092+ivohas@users.noreply.github.com>
2023-01-21 10:39:40 +0200

    Create Program.cs

---
+++ 03. Conditional checks/Excellent_Result/InvalidNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace InvalidNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            if (num>=100&&num<=200)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
