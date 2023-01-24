a1823724bc4da0c303fcfc11d91c5b0fcfcde2bd
ivohas <105879092+ivohas@users.noreply.github.com>
2023-01-24 19:15:40 +0200

    Create Program.cs

---
+++ 03. Conditional checks/Excellent_Result/NumberToWords_100/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+namespace NumberToWords_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num==25)
+            {
+                Console.WriteLine("twenty five");
+            }
+            if (num == 12) {
+                Console.WriteLine("twelve");
+            }
+            if (num==99) {
+                Console.WriteLine("ninety nine");
+            }
+            if (num==50)
+            {
+                Console.WriteLine("fifty");
+            }
+            if (num==0) {
+                Console.WriteLine("zero");
+            }
+            if (num==1)
+            {
+                Console.WriteLine("one");
+            }
+            if (num==2)
+            {
+                Console.WriteLine("two");
+            }
+            if (num==10)
+            {
+                Console.WriteLine("ten");
+            }
+            if (num==11)
+            {
+                Console.WriteLine("eleven");
+            }
+            if (num==13)
+            {
+                Console.WriteLine("thirteen");
+            }
+            if (num==15)
+            {
+                Console.WriteLine("fifteen");
+            }
+            if (num==19)
+            {
+                Console.WriteLine("nineteen");
+            }
+            if (num==20)
+            {
+                Console.WriteLine("twenty");
+            }
+            if (num==21)
+            {
+                Console.WriteLine("twenty one");
+            }
+            if (num==30)
+            {
+                Console.WriteLine("thirty");
+            }
+            if (num==41)
+            {
+                Console.WriteLine("forty one");
+            }
+            if (num==52)
+            {
+                Console.WriteLine("fifty two");
+            }
+            if (num==63)
+            {
+                Console.WriteLine("sixty three");
+            }
+            if (num==17)
+            {
+                Console.WriteLine("seventy four");
+            }
+        }
+    }
+}
