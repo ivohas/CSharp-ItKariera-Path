fdbabe55574eca4ed47afc7f2cdd663d18979654
ivohas <105879092+ivohas@users.noreply.github.com>
2023-01-24 19:14:48 +0200

    Create Program.cs

---
+++ 03. Conditional checks/Excellent_Result/NumberToWords/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace NumberToWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            switch (num) {
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+                case 2: Console.WriteLine("two");break;
+                case 3: Console.WriteLine("three"); break;
+                case 4: Console.WriteLine("four"); break;
+                case 5: Console.WriteLine("five"); break;
+                case 6: Console.WriteLine("six"); break;
+                case 7: Console.WriteLine("seven"); break;
+                case 8: Console.WriteLine("eight"); break;
+                case 9: Console.WriteLine("nine"); break;
+                default:
+                    Console.WriteLine("number too big");
+                    break;
+            
+            }
+        }
+    }
+}
