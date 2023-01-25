b4ed8f902927e257ec78ba5f230c5f76f4123c34
ivohas <105879092+ivohas@users.noreply.github.com>
2023-01-25 16:24:10 +0200

    Create Program.cs

---
+++ 03. Conditional checks/Excellent_Result/Odd_Or_Even/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Odd_Or_Even
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
