8b493b5e9ddcc70a048f52a89a0995a87b7f1598
ivohas <105879092+ivohas@users.noreply.github.com>
2023-01-26 16:56:52 +0200

    Create Program.cs

---
+++ 03. Conditional checks/Excellent_Result/PersonalTitles/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace PersonalTitles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var age = double .Parse(Console.ReadLine());
+            string sex=Console.ReadLine();
+            if (sex=="m")
+            {
+                if (age>=16)
+                {
+                    Console.WriteLine("Mr.");
+                }
+                else
+                {
+                    Console.WriteLine("Master");
+                }
+            }
+            else if (sex=="f")
+            {
+                if (age>=16)
+                {
+                    Console.WriteLine("Ms.");
+                }
+                else
+                {
+                    Console.WriteLine("Miss");
+                }
+            }
+
+        }
+    }
+}
