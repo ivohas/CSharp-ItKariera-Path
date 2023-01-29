ea380f66d42680a56f27b00a3f818d94fad50492
ivohas <105879092+ivohas@users.noreply.github.com>
2023-01-29 10:17:24 +0200

    Create Program.cs

---
+++ 03. Conditional checks/Excellent_Result/ThreeEqualNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace ThreeEqualNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3= int.Parse(Console.ReadLine());
+            if (num1 == num2)
+            {
+                if (num2 == num3)
+                {
+                    Console.WriteLine("yes");
+                }
+                else
+                {
+                    Console.WriteLine("no");
+                }
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
