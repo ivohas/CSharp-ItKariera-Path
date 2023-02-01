2e49f226725d05e869ac96b7b0fbc0bdd6175e26
ivohas <105879092+ivohas@users.noreply.github.com>
2023-02-01 16:32:51 +0200

    Create Program.cs

---
+++ 04. Loops/Loop/EvenOddSums/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace EvenOddSums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sumOdd = 0;
+            int sumEven = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int num= int.Parse(Console.ReadLine());
+                if (i % 2 == 0) {
+                    sumOdd += num;
+                }
+                else
+                {
+                    sumEven += num;
+                }
+            }
+            if (sumEven==sumOdd)
+            {
+                Console.WriteLine($"Yes Sum = {sumOdd }");
+            }
+            else
+            {
+                Console.WriteLine($"No Diff = {Math.Abs(sumEven-sumOdd)}");
+            }
+        }
+    }
+}
