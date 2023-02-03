64c34e3633eb1a2e43a9a7d5147c39e85c8f97ed
ivohas <105879092+ivohas@users.noreply.github.com>
2023-02-03 12:39:26 +0200

    Create Program.cs

---
+++ 04. Loops/Loop/LatinLetters/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace LatinLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "Latin alphabet:";
+            for (int i = 'a'; i <= 'z'; i++)
+            {
+                char letter = (char)i;
+                result += $" {letter}";
+            }
+            Console.WriteLine(result.Trim());
+        }
+    }
+}
