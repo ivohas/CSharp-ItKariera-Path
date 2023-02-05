0b27e5bd4bf90c6dd4f9efc16888c8ed32e87b1c
ivohas <105879092+ivohas@users.noreply.github.com>
2023-02-05 12:20:44 +0200

    Create Program.cs

---
+++ 04. Loops/Loop/Loop/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
