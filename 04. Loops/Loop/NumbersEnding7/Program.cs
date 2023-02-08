c18693239f10c5ef1c5c97ec7b5db1148555f1ed
ivohas <105879092+ivohas@users.noreply.github.com>
2023-02-08 20:43:15 +0200

    Create Program.cs

---
+++ 04. Loops/Loop/NumbersEnding7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace NumbersEnding7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 7; i < 1000; i+=10)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
