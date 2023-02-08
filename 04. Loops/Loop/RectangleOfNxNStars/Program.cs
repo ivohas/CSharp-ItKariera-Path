42a02130862fdfe1c21cb30b1c3a50a856e86e22
ivohas <105879092+ivohas@users.noreply.github.com>
2023-02-08 20:43:29 +0200

    Create Program.cs

---
+++ 04. Loops/Loop/RectangleOfNxNStars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace RectangleOfNxNStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string result = "";
+                for (int a = 0; a < n; a++)
+                {
+                    result += "* ";
+                }
+                Console.WriteLine(result.TrimEnd());
+            }
+        }
+    }
+}
