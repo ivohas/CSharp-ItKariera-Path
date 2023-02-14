4ef5693199f0a5e1b378c345cd7620a136a78276
ivohas <105879092+ivohas@users.noreply.github.com>
2023-02-14 18:37:34 +0200

    Create Program.cs

---
+++ 04. Loops/Loop/UnitConverter/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        double num = double.Parse(Console.ReadLine());
+        string first = Console.ReadLine();
+        string second = Console.ReadLine();
+
+        switch (first)
+        {
+            case "mm":
+                num /= 1000;
+                break;
+            case "cm":
+                num /= 100;
+                break;
+            case "mi":
+                num /= 0.000621371192;
+                break;
+            case "in":
+                num /= 39.3700787;
+                break;
+            case "km":
+                num = 1000;
+                break;
+            case "ft":
+                num /= 3.2808399;
+                break;
+            case "yd":
+                num /= 1.0936133;
+                break;
+        }
+
+        switch (second)
+        {
+            case "mm":
+                num = 1000;
+                break;
+            case "cm":
+                num = 100;
+                break;
+            case "mi":
+                num = 0.000621371192;
+                break;
+            case "in":
+                num = 39.3700787;
+                break;
+            case "km":
+                num /= 1000;
+                break;
+            case "ft":
+                num = 3.2808399;
+                break;
+            case "yd":
+                num *= 1.0936133;
+                break;
+        }
+
+        Console.WriteLine($"{num} {second}");
+    }
+}
