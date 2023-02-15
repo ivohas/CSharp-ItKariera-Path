d799ada61e89d2ccac274a362d0a7f559d35ac44
ivohas <105879092+ivohas@users.noreply.github.com>
2023-02-15 19:00:39 +0200

    Create Program.cs

---
+++ 04. Loops/Loops Exersise/EvenNumberWithCheck/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace EvenNumberWithCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 0;
+            while (true)
+            {
+                Console.Write("Enter even number :");
+
+                n = int.Parse(Console.ReadLine());
+                if (n % 2 == 0)
+                    break; // even number --> exit from the loop
+            }
+            Console.WriteLine("The number is not even");
+            Console.WriteLine("Even number entered : {0}", n);
+        }
+    }
+}
